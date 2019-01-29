422f8efd311efa346074df11bd63c742f5ffb1f6
Paul Schaefer <yeppaul2@gmail.com>
2019-01-29 13:19:21 -0500

    Added barby-powered class for generating barcode from location.

--- app.rb
+++ app.rb
@@ -4,8 +4,14 @@ require 'bundler/setup'
 # Load the gems
 require 'sinatra'
 
+# Require my helpers
+require_relative 'lib/barcode'
+
 # On '/' page, do this...
 get '/' do
+  # Generates a barcode from location, then returns it to the page
+  @barcode = BarcodeHelper.barcode('L01A001A01')
+
   # Render views/index.erb
   erb :index
 end
---
+++ lib/barcode.rb
@@ -0,0 +1,13 @@
+# Require barby and its dependencies
+require 'barby'
+require 'barby/barcode/code_39'
+require 'barby/outputter/svg_outputter'
+
+# Generate barcode from location
+class BarcodeHelper
+  puts '== BarcodeHelper loaded'
+  def self.barcode(location)
+    puts '== Barcode function loaded'
+    Barby::Code39.new(location)
+  end
+end
